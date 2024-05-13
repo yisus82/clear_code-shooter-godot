af32b6e6b4351eed67fcd9c3693e480db62a7fdc
yisus82 <djalma8@gmail.com>
2024-05-13 11:01:03 +0200

    Add shooting timers

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,19 +1,28 @@
 extends CharacterBody2D
 
 const speed: int = 500
+var can_shoot_laser: bool = true
+var can_shoot_grenade: bool = true
 
-func _ready():
-	pass
-
-
-func _process(delta):
+func _process(_delta):
 	# movement input
 	var movement: Vector2 = Input.get_vector("left", "right", "up", "down")
 	velocity = movement * speed
 	move_and_slide()
 
 	# shooting input
-	if Input.is_action_pressed("fire1"):
+	if Input.is_action_pressed("fire1") and can_shoot_laser:
 		print("shoot laser")
-	if Input.is_action_pressed("fire2"):
+		can_shoot_laser = false
+		$LaserTimer.start()
+	if Input.is_action_pressed("fire2") and can_shoot_grenade:
 		print("shoot grenade")
+		can_shoot_grenade = false
+		$GrenadeTimer.start()
+
+func _on_laser_timer_timeout():
+	can_shoot_laser = true
+
+
+func _on_grenade_timer_timeout():
+	can_shoot_grenade = true
