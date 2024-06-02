cdf1e74bbb10a4b924785b3db0bd508c6c2fdbcf
yisus82 <djalma8@gmail.com>
2024-06-03 01:03:30 +0200

    Level transitions

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -3,4 +3,4 @@ extends LevelParent
 func _on_exit_gate_area_body_entered(_body: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property($Player,"speed",0,0.5)
-	get_tree().change_scene_to_file.bind("res://scenes/levels/outside.tscn").call_deferred()
+	TransitionLayer.change_scene("res://scenes/levels/outside.tscn")
